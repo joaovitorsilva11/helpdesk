26cf6eca0811d65c01a2abbc1f25bdb10f0575d6
joaovitorsilva11 <162445983+joaovitorsilva11@users.noreply.github.com>
2025-05-12 11:15:45 -0300

    import

--- helpdesk-backend-master/src/main/java/com/marcelo/helpdesk/config/SecutiryConfig.java
+++ helpdesk-backend-master/src/main/java/com/marcelo/helpdesk/config/SecutiryConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
