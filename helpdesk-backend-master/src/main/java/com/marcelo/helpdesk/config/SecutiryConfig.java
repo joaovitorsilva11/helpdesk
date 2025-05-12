05bd565f0056d30fbb1b5d62f84d189178043ef8
joaovitorsilva11 <162445983+joaovitorsilva11@users.noreply.github.com>
2025-05-12 11:40:22 -0300

    .

--- helpdesk-backend-master/src/main/java/com/marcelo/helpdesk/config/SecutiryConfig.java
+++ helpdesk-backend-master/src/main/java/com/marcelo/helpdesk/config/SecutiryConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-import org.springframework.security.authentication.AuthenticationManager;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -90,9 +90,4 @@ CorsConfigurationSource corsConfigurationSource() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
 }
